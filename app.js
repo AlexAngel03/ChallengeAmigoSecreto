39d90ca7bf9f806ec69cbecf2f26916f3bfaf88e
Fabiana <thiswhere@gmail.com>
2025-09-18 11:38:46 -0500

    inicializando repositorio

---
+++ app.js
@@ -0,0 +1,19 @@
+//variables.
+//seleccionar un elemento del html
+
+const inputAmigo=document.getElementById
+("amigo");
+const listaAmigos=[];
+const ulListaAmigos=document.getElementById("listaAmigos");
+const ulResultado=document.getElementById("resultado");
+function agregarAmigo(){
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML +=  `<li>${inputAmigo.value}</li>`;
+};
+
+function sortearAmigo(){
+const random = Math.floor(Math.random()*listaAmigos.length);
+const amigoSecreto = listaAmigos{random};
+ulResultado.innerHTML=`<li>${amigoSecreto}</li>`;
+
+}
